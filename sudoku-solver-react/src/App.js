cf45b89619e80dcdd6cce81fa93fbac65fb5c304
unknown <malemlink@gmail.com>
2023-01-08 20:25:37 +0530

    functional

--- sudoku-solver-react/src/App.js
+++ sudoku-solver-react/src/App.js
@@ -13,13 +13,36 @@ const inputBoardOriginal =
   ['', '', '', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '', '', '']]
 
+const hardCodedSudoku =
+  [['5', '3', '', '', '7', '', '', '', ''],
+  ['6', '', '', '1', '9', '5', '', '', ''],
+  ['', '9', '8', '', '', '', '', '6', ''],
+  ['8', '', '', '', '6', '', '', '', '3'],
+  ['4', '', '', '8', '', '3', '', '', '1'],
+  ['7', '', '', '', '2', '', '', '', '6'],
+  ['', '6', '', '', '', '', '2', '8', ''],
+  ['', '', '', '4', '1', '9', '', '', '5'],
+  ['', '', '', '', '8', '', '', '7', '9']]
+
+/**
+ * Cell React component
+ * @param {*} props 
+ * @returns 
+ */
 function Cell(props) {
   return (
     <input type="text" className="square" id={`cell${props.x}${props.y}`} name={`cell${props.x}${props.y}`} defaultValue={props.value} readOnly={props.readOnly} style={props.fixedCell ? { backgroundColor: 'grey' } : { backgroundColor: null }} />
   );
 }
 
+/**
+ * Board React component
+ */
 class Board extends React.Component {
+  /**
+   * Input form submission handler
+   * @param {*} event 
+   */
   onSubmit(event) {
     // obtain input
     event.preventDefault();
@@ -33,6 +56,14 @@ class Board extends React.Component {
     this.props.onSubmit(JSON.parse(JSON.stringify(inputBoardOriginal)));
   }
 
+  /**
+   * Render each cell of the sudoku board
+   * @param {*} x 
+   * @param {*} y 
+   * @param {*} value 
+   * @param {*} readOnly 
+   * @returns 
+   */
   renderCell(x, y, value, readOnly) {
     return (
       <Cell
@@ -48,7 +79,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        {this.props?.solutionNum > 0 && <p>Solution: {this.props.solutionCount}</p>}
+        {this.props?.solutionNum > 0 && <p>Solution: {this.props.solutionNum}</p>}
         <form onSubmit={(event) => this.onSubmit(event)}>
           {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((i, iindex) => {
 
@@ -75,10 +106,14 @@ class Board extends React.Component {
   }
 }
 
+/**
+ * Root component
+ */
 class Sudoku extends React.Component {
   constructor(props) {
     super(props);
-    this.emptyBoard =
+
+    this.inputBoard =
       [['', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', ''],
@@ -89,20 +124,54 @@ class Sudoku extends React.Component {
       ['', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '']]
 
+    // for testing
+    this.inputBoard = hardCodedSudoku;
+
+    this.solutions = []
+
     this.state = {
-      board: JSON.parse(JSON.stringify(this.emptyBoard)),
+      board: JSON.parse(JSON.stringify(this.inputBoard)),
       solutionCount: 0,
       invalidBoard: false
     }
   }
 
+  /**
+   * To check consistency on input sudoku
+   * @returns 
+   */
+  checkBoardConsistency() {
+    let count = 0;
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (!['', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(this.inputBoard[i][j])) {
+          return false;
+        }
+        if (this.inputBoard[i][j] !== '') {
+          count++
+        }
+      }
+    }
+
+    if (count < 20) {
+      // system might crash if tried to generate solution for such cases
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Handle sudoku submission
+   * @param {*} inputBoard 
+   * @returns 
+   */
   handleSubmit(inputBoard) {
+    this.inputBoard = inputBoard
 
     // validate input board; do not continue if board is invalid
-    if (!checkBoardConsistency(inputBoard)) {
+    if (!this.checkBoardConsistency()) {
       clearSolutions();
       this.setState({
-        board: inputBoard,
         invalidBoard: true
       })
       return;
@@ -111,7 +180,12 @@ class Sudoku extends React.Component {
     clearSolutions();
 
     // solve sudoku
-    solve(inputBoard, this.state.solutionCount);
+    this.solve();
+
+    // display solutions
+    for (let i = 0; i < this.solutions.length; i++) {
+      displaySolution(this.solutions[i], i + 1)
+    }
 
     if (this.state.invalidBoard)
       this.setState({
@@ -120,19 +194,97 @@ class Sudoku extends React.Component {
 
   }
 
+  /**
+   * solve - recursive backtracking algorithm
+   * @returns 
+   */
+  solve() {
+    for (let x = 0; x < 9; x++) {
+      for (let y = 0; y < 9; y++) {
+        if (this.inputBoard[x][y] === '') {
+          for (let value = 1; value <= 9; value++) {
+            if (this.canWePut(x, y, '' + value)) {
+              this.inputBoard[x][y] = '' + value
+              this.solve()
+              this.inputBoard[x][y] = ''
+            }
+          }
+          return;
+        }
+      }
+    }
+
+    // save the solution
+    this.solutions.push(JSON.parse(JSON.stringify(this.inputBoard)))
+  }
+
+  /**
+   * canWePut - to check if a value can be put in a given position
+   * @param {*} x 
+   * @param {*} y 
+   * @param {*} value 
+   * @returns 
+   */
+  canWePut(x, y, value) {
+    // check rows
+    for (let i = 0; i < 9; i++) {
+      if (this.inputBoard[x][i] === value) {
+        return false
+      }
+    }
+
+    // check columns
+    for (let j = 0; j < 9; j++) {
+      if (this.inputBoard[j][y] === value) {
+        return false
+      }
+    }
+
+    // check the sub-square
+    let x_start = Math.floor(x / 3) * 3
+    let y_start = Math.floor(y / 3) * 3
+    for (let i = x_start; i < x_start + 3; i++) {
+      for (let j = y_start; j < y_start + 3; j++) {
+        if (this.inputBoard[i][j] === value) {
+          return false
+        }
+      }
+    }
+
+    return true
+  }
+
+  /**
+   * Reset the board.
+   */
   resetBoard() {
     clearSolutions();
     this.setState({
       invalidBoard: false
     })
     // below is not working
-    // let emptyBoard = JSON.parse(JSON.stringify(this.emptyBoard))
+    // let emptyBoard = JSON.parse(JSON.stringify(hardCodedSudoku))
     // this.setState({
     //   board: emptyBoard
     // })
+
+    // this is a work around
+    // for (let i = 0; i < 9; i++) {
+    //   for (let j = 0; j < 9; j++) {
+    //     const cell = document.getElementById(`cell${i}${j}`);
+    //     cell.value = hardCodedSudoku[i][j]
+    //   }
+    // }
     const cells = document.getElementsByClassName("square");
     for (let i = 0; i < cells.length; i++) {
       cells[i].value = ''
+    }
+
+    // reset inputBoardOriginal
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        inputBoardOriginal[i][j] = ''
+      }
     }
   }
 
@@ -158,10 +310,11 @@ class Sudoku extends React.Component {
   }
 }
 
-function solve(inputBoard, solutionCount) {
-  displaySolution(inputBoard, solutionCount);
-}
-
+/**
+ * Creates new DIVs and appends solution boards
+ * @param {*} board 
+ * @param {*} solutionCount 
+ */
 function displaySolution(board, solutionCount) {
   const solutionsDiv = document.getElementById('solutions')
   const solution = document.createElement("div")
@@ -169,8 +322,8 @@ function displaySolution(board, solutionCount) {
   solution.id = solutionId
   solutionsDiv.appendChild(solution)
 
-  const root = ReactDOM.createRoot(document.getElementById(solutionId))
-  root.render(
+  const solutionComp = ReactDOM.createRoot(document.getElementById(solutionId))
+  solutionComp.render(
     <React.StrictMode>
       <Board
         boardContent={board}
@@ -181,17 +334,9 @@ function displaySolution(board, solutionCount) {
   )
 }
 
-function checkBoardConsistency(board) {
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (!['', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(board[i][j])) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
+/**
+ * Remove the solution boards/DIVs
+ */
 function clearSolutions() {
   // clear previous solution from UI
   const solutionsDiv = document.getElementById('solutions')
